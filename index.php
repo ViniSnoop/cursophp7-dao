e409f89c8384498fde44d7ffbd3592017274a3e6
Vinícius Fernandes de Souza <vinicius.fernandes2000@outlook.com>
2022-01-27 21:32:21 -0300

    Metodos de lista

--- index.php
+++ index.php
@@ -2,18 +2,32 @@
 
 require_once("config.php");
 
-$jose = new Usuario();
+//Carrega um usuario
 
+/*$jose = new Usuario();
 $jose->loadById(4);
+echo $jose;*/
 
-echo $jose;
+//Carrega uma list a de usuarios
+
+/*$lista = Usuario::getList();
+echo json_encode($lista);*/
+
+//Carrega uma lista de usuarios buscando pelo login
+
+/*$search = Usuario::search("jo");
+echo json_encode($search);*/
+
+//Carrega um usuario usando o login e a senha 
+
+$usuario = new Usuario();
+$usuario->login("root", "123456");
+
+echo $usuario;
 
 /*
 $sql = new Sql();
-
 $usuarios = $sql->select("SELECT * FROM tb_usuarios");
-
 echo json_encode($usuarios);
 */
-
 ?>
